f87530ed4dc659aa828bc16e4b293bdf242a4086
Jeandra <jeandra8789@gmail.com>
2022-06-09 15:53:17 -0300

    projeto finalizado

---
+++ js/scripts.js
@@ -0,0 +1,133 @@
+const addressForm = document.querySelector("#address-form");
+const cepInput = document.querySelector("#cep");
+const addressInput = document.querySelector("#address");
+const cityInput = document.querySelector("#city");
+const neighborhoodInput = document.querySelector("#neighborhood");
+const regionInput = document.querySelector("#region");
+const formInputs = document.querySelectorAll("[data-input]");
+
+const closeButton = document.querySelector("#close-message");
+
+const fadeElement = document.querySelector("#fade");
+
+// Validando CEP Input
+cepInput.addEventListener("keypress", (e) => {
+    const onlyNumbers = /[0-9]|./;
+    const key = String.fromCharCode(e.KeyCode);
+
+    console.log(key);
+
+    console.log(onlyNumbers.test(key));
+
+    // allow only numbers
+    if (!onlyNumbers.test(key)) {
+        e.preventDefault();
+        return;
+    }
+});
+
+// Event to get address 
+cepInput.addEventListener("keyup", (e) => {
+    const inputValue = e.target.value;
+
+    // Check if we have address
+    if (inputValue.length === 8) {
+        getAddress(inputValue);
+    }
+});
+
+// Get address from API
+const getAddress = async (cep) => {
+    toggleLoader();
+
+    cepInput.blur();
+
+    const apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
+
+    const response = await fetch(apiUrl);
+
+    const data = await response.json();
+
+    console.log(data);
+    console.log(formInputs);
+    console.log(data.erro);
+
+    // show error and reset form
+    if (data.erro === "true") {
+        if (!addressInput.hasAttribute("disabled")) {
+            toggleDisabled();
+        }
+
+        addressForm.reset();
+        toggleLoader();
+        toggleMessage("CEP Inválido, tente novamente.");
+        return;
+    }
+
+    // Activate disabled attribute if form is empty
+    if (addressInput.value === "") {
+        toggleDisabled();
+    }
+
+    addressInput.value = data.logradouro;
+    cityInput.value = data.localidade;
+    neighborhoodInput.value = data.bairro;
+    regionInput.value = data.uf;
+
+    toggleLoader();
+};
+
+// Add or remove disable attribute 
+const toggleDisabled = () => {
+    if (regionInput.hasAttribute("disabled")) {
+        formInputs.forEach((input) => {
+            input.removeAttribute("disabled");
+        });
+    } else {
+        formInputs.forEach((input) => {
+            input.setAttribute("disabled", "disabled")
+        });
+    }
+};
+
+// Show or hide loader
+const toggleLoader = () => {
+    
+    const loaderElement = document.querySelector("#loader");
+
+    fadeElement.classList.toggle("hide");
+    loaderElement.classList.toggle("hide");
+};
+
+// Show or hide message 
+const toggleMessage = (msg) => {
+    const fadeElement = document.querySelector("#fade");
+    const messageElemnt = document.querySelector("#message");
+
+    const messageTextElement = document.querySelector("#message p");
+
+    messageTextElement.innerText = msg;
+
+    fadeElement.classList.toggle("hide");
+    messageTextElement.classList.toggle("hide");
+};
+
+// Close message modal
+closeButton.addEventListener("clock", () => toggleMessage());
+
+// Save address 
+addressForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    toggleLoader();
+
+    setTimeout(() => {
+        toggleLoader();
+
+        toggleMessage("Endereço salvo com sucesso!");
+
+        addressForm.reset();
+
+        toggleDisabled();
+    }, 1000);
+});
